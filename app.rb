44f6a90b8a70361908af4e913b0e3521a8a1d3c8
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-18 23:48:53 +0000

    rps simple finished

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get ("/rock") do
+  @our_move = "rock"
+  moves = ["rock", "paper", "scissors"]
+  @your_move = moves.sample
+
+  if @your_move == "rock"
+    @outcome = "tied"
+  elsif @your_move == "paper"
+    @outcome = "lost"
+  elsif @your_move == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:rps)
+end
+
+get ("/paper") do
+  @our_move = "paper"
+  moves = ["rock", "paper", "scissors"]
+  @your_move = moves.sample
+
+  if @your_move == "rock"
+    @outcome = "won"
+  elsif @your_move == "paper"
+    @outcome = "tied"
+  elsif @your_move == "scissors"
+    @outcome = "lost"
+  end
+
+  erb(:rps)
+end
+
+get ("/scissors") do
+  @our_move = "scissors"
+  moves = ["rock", "paper", "scissors"]
+  @your_move = moves.sample
+
+  if @your_move == "rock"
+    @outcome = "lost"
+  elsif @your_move == "paper"
+    @outcome = "won"
+  elsif @your_move == "scissors"
+    @outcome = "tied"
+  end
+
+  erb(:rps)
 end
